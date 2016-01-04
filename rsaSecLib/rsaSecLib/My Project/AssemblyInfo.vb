13affa27c6d1625db74030f9310b910e0d945355
master-m1000 <mario.wagenknecht@gmai.com>
2016-01-05 00:10:13 +0100

    First submit

---
+++ rsaSecLib/rsaSecLib/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' Allgemeine Informationen über eine Assembly werden über die folgenden 
+' Attribute gesteuert. Ändern Sie diese Attributwerte, um die Informationen zu ändern,
+' die einer Assembly zugeordnet sind.
+
+' Werte der Assemblyattribute überprüfen
+
+<Assembly: AssemblyTitle("rsaSecLib")> 
+<Assembly: AssemblyDescription("")> 
+<Assembly: AssemblyCompany("")> 
+<Assembly: AssemblyProduct("rsaSecLib")> 
+<Assembly: AssemblyCopyright("Copyright ©  2016")> 
+<Assembly: AssemblyTrademark("")> 
+
+<Assembly: ComVisible(False)>
+
+'Die folgende GUID bestimmt die ID der Typbibliothek, wenn dieses Projekt für COM verfügbar gemacht wird.
+<Assembly: Guid("023aed4a-22f4-4f1c-9332-11b0006cc5cd")> 
+
+' Versionsinformationen für eine Assembly bestehen aus den folgenden vier Werten:
+'
+'      Hauptversion
+'      Nebenversion 
+'      Buildnummer
+'      Revision
+'
+' Sie können alle Werte angeben oder die standardmäßigen Build- und Revisionsnummern 
+' übernehmen, indem Sie "*" eingeben:
+' <Assembly: AssemblyVersion("1.0.*")> 
+
+<Assembly: AssemblyVersion("1.0.0.0")> 
+<Assembly: AssemblyFileVersion("1.0.0.0")> 
---
+++ rsaSecLib/rsaSecLib/rsaSecLib.vb
@@ -0,0 +1,114 @@
+﻿Imports System.Security.Cryptography
+Imports System.Text
+
+Public Class rsaSecLib
+
+    Structure KeyPair
+        ''' <summary>
+        ''' Public key as XML
+        ''' </summary>
+        ''' <returns></returns>
+        Property publicKey As String
+
+        ''' <summary>
+        ''' Private key as XML
+        ''' </summary>
+        ''' <returns></returns>
+        Property privateKey As String
+    End Structure
+
+
+    ''' <summary>
+    ''' Creates a random RSA key pair.
+    ''' </summary>
+    ''' <param name="keysize">Key size as a power of 2, e.g. 2048 or 4096</param>
+    ''' <returns></returns>
+    Function CreateKeyPair(ByVal keysize As Integer) As KeyPair
+        Dim rsaKey As New KeyPair 'Declare return value
+        Dim rsaCryptoProvider As New RSACryptoServiceProvider(keysize) 'Declare RSA service and generate random key pair
+        rsaKey.publicKey = rsaCryptoProvider.ToXmlString(False) 'Public key to XML
+        rsaKey.privateKey = rsaCryptoProvider.ToXmlString(True) 'Private key to XML
+        rsaCryptoProvider.Dispose() 'Deallocate ressources
+        Return rsaKey
+    End Function
+
+    ''' <summary>
+    ''' Generates the public key by the private key.
+    ''' </summary>
+    ''' <param name="privateKeyXml"></param>
+    ''' <param name="keysize"></param>
+    ''' <returns></returns>
+    Function GeneratePublicKeyWithPrivateKey(ByVal privateKeyXml As String, ByVal keysize As Integer) As String
+        Dim rsaCryptoProvider As New RSACryptoServiceProvider(keysize) 'Declare RSA service
+        Dim publicKey As String 'Declare return value
+        rsaCryptoProvider.FromXmlString(privateKeyXml) 'RSA service receives private key
+        publicKey = rsaCryptoProvider.ToXmlString(False) 'Public key to XML
+        Return publicKey
+    End Function
+
+    ''' <summary>
+    ''' Encrypts a String.
+    ''' </summary>
+    ''' <param name="DATA">String to encrypt</param>
+    ''' <param name="publicKeyXml">Public key as XML</param>
+    ''' <param name="keysize">Key size as a power of 2, e.g. 2048 or 4096</param>
+    ''' <returns>Encrypted String</returns>
+    Function EncryptString(ByVal DATA As String, ByVal publicKeyXml As String, ByVal keysize As Integer) As String
+        Dim rsaCryptoProvider As New RSACryptoServiceProvider(keysize) 'Declare RSA service
+        Dim byteData As Byte() = Encoding.UTF8.GetBytes(DATA)  'Write data string into a Byte Array
+        Dim encryptedString As String = String.Empty 'Declare return value
+        rsaCryptoProvider.FromXmlString(publicKeyXml) 'RSA service receives public key
+        encryptedString = Convert.ToBase64String(rsaCryptoProvider.Encrypt(byteData, True)) 'Encrypts data and write it into a Base64 string
+        rsaCryptoProvider.Dispose() 'Deallocate ressources
+        Return encryptedString
+    End Function
+
+    ''' <summary>
+    ''' Decrypts a String.
+    ''' </summary>
+    ''' <param name="DATA">String to dencrypt</param>
+    ''' <param name="privateKeyXml">Private key as XML</param>
+    ''' <param name="keysize">Key size as a power of 2, e.g. 2048 or 4096</param>
+    ''' <returns>Decrypted String</returns>
+    Function DecryptString(ByVal DATA As String, ByVal privateKeyXml As String, ByVal keysize As Integer) As String
+        Dim rsaCryptoProvider As New RSACryptoServiceProvider(keysize) 'Declare RSA service
+        Dim byteData As Byte() = Convert.FromBase64String(DATA) 'Write data Base64 string into a Byte Array
+        Dim decryptedString As String = String.Empty 'Declare return value
+        rsaCryptoProvider.FromXmlString(privateKeyXml) 'RSA service receives private key
+        decryptedString = Encoding.Default.GetString(rsaCryptoProvider.Decrypt(byteData, True)) 'Encrypts data and write it into the return value
+        rsaCryptoProvider.Dispose() 'Deallocate ressources
+        Return decryptedString
+    End Function
+
+    ''' <summary>
+    ''' Encrypts a Byte Array.
+    ''' </summary>
+    ''' <param name="DATA">Byte Array to encrypt</param>
+    ''' <param name="publicKeyXml">Public key as XML</param>
+    ''' <param name="keysize">Key size as a power of 2, e.g. 2048 or 4096</param>
+    ''' <returns>Encrypted Byte Array</returns>
+    Function EncryptByte(ByVal DATA As Byte(), ByVal publicKeyXml As String, ByVal keysize As Integer) As Byte()
+        Dim rsaCryptoProvider As New RSACryptoServiceProvider(keysize) 'Declare RSA service
+        Dim encryptedDATA As Byte() = {0} 'Declare return value
+        rsaCryptoProvider.FromXmlString(publicKeyXml) 'RSA service receives public key
+        encryptedDATA = rsaCryptoProvider.Encrypt(DATA, True) 'Encrypts data and write it into a Base64 string
+        rsaCryptoProvider.Dispose() 'Deallocate ressources
+        Return encryptedDATA
+    End Function
+
+    ''' <summary>
+    ''' Decrypts a Byte Array.
+    ''' </summary>
+    ''' <param name="DATA">Byte Array to dencrypt</param>
+    ''' <param name="privateKeyXml">Private key as XML</param>
+    ''' <param name="keysize">Key size as a power of 2, e.g. 2048 or 4096</param>
+    ''' <returns>Decrypted Byte Array</returns>
+    Function DecryptByte(ByVal DATA As Byte(), ByVal privateKeyXml As String, ByVal keysize As Integer) As Byte()
+        Dim rsaCryptoProvider As New RSACryptoServiceProvider(keysize) 'Declare RSA service
+        Dim decryptedDATA As Byte() = {0} 'Declare return value
+        rsaCryptoProvider.FromXmlString(privateKeyXml) 'RSA service receives private key
+        decryptedDATA = rsaCryptoProvider.Decrypt(DATA, True) 'Encrypts data and write it into the return value
+        rsaCryptoProvider.Dispose() 'Deallocate ressources
+        Return decryptedDATA
+    End Function
+End Class
